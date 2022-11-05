29a5a69ee0b7b15e82ebebbbdfacfbadff19de82
rickyputrasardika22 <primatara42@gmail.com>
2022-11-05 12:31:15 +0700

    Commit

--- DasarOOP-1/src/com/belajaroop1/Main.java
+++ DasarOOP-1/src/com/belajaroop1/Main.java
@@ -1,13 +1,34 @@
 package com.belajaroop1;
 
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        //INISIALISASI OBJEK BARU
-        TampilBiodata mahasiswa1 = new TampilBiodata("Prima", "Putra", 22, "Teknik Informatika");
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Nama saya adalah " + mahasiswa1.namaAwal + " " + mahasiswa1.namaAkhir);
-        System.out.println("Saya berusia " + mahasiswa1.usia + " tahun dan merupakan mahasiswa prodi " + mahasiswa1.prodi);
+        //Inisialisasi Property Untuk Objek
+        String inputNamaAwal, inputNamaAkhir, inputTanggalLahir, inputProdi;
+
+        System.out.print("Masukkan Nama Awal \t\t: ");
+        inputNamaAwal = input.nextLine();
+
+        System.out.print("Masukkan Nama Akhir \t: ");
+        inputNamaAkhir = input.nextLine();
+
+        System.out.print("Masukkan Tanggal Lahir \t: ");
+        inputTanggalLahir = input.nextLine();
+
+        System.out.print("Masukkan Prodi \t\t\t: ");
+        inputProdi = input.nextLine();
+
+        //Inisialisasi Objek
+        TampilBiodata mahasiswa1 = new TampilBiodata(inputNamaAwal, inputNamaAkhir, inputTanggalLahir, inputProdi);
+        TampilBiodata mahasiswa2 = new TampilBiodata("Rianto", "Julian", "3 Agustus 2001", "Teknik Mesin");
+
+        //JALANKAN METHOD INSTANCE
+        mahasiswa1.printBiodata();
+        mahasiswa2.printBiodata();
     }
 }
--- DasarOOP-1/src/com/belajaroop1/TampilBiodata.java
+++ DasarOOP-1/src/com/belajaroop1/TampilBiodata.java
@@ -2,15 +2,26 @@ package com.belajaroop1;
 
 public class TampilBiodata
 {
-    public String namaAwal, namaAkhir, prodi;
-    public int usia;
+    public String namaAwal, namaAkhir, namaLengkap, tanggalLahir, prodi;
+
 
     //Constructor
-    TampilBiodata(String namaAwal, String namaAkhir, int usia, String prodi)
+    TampilBiodata(String inputNamaAwal, String inputNamaAkhir, String inputTanggalLahir, String inputProdi)
     {
-        this.namaAwal = namaAwal;
-        this.namaAkhir = namaAkhir;
-        this.usia = usia;
-        this.prodi = prodi;
+        this.namaAwal = inputNamaAwal;
+        this.namaAkhir = inputNamaAkhir;
+        namaLengkap = inputNamaAwal + " " + inputNamaAkhir;
+        this.tanggalLahir = inputTanggalLahir;
+        this.prodi = inputProdi;
+    }
+
+    //Method Untuk Menampilkan Nama Lengkap
+    void printBiodata()
+    {
+        System.out.println();
+        System.out.println("Nama \t\t\t: " + this.namaLengkap);
+        System.out.println("Tanggal Lahir \t: " + this.tanggalLahir);
+        System.out.println("Prodi \t\t\t: " + this.prodi);
+        System.out.println();
     }
 }
